ed98ac60a240f588f5fb9b41d74b352e2890b7fd
orbitly17 <72365083+orbitly17@users.noreply.github.com>
2021-11-16 14:53:51 +0800

    Initial commit

---
+++ src/form.ts
@@ -0,0 +1,63 @@
+export interface LoginForm {
+    username?: string;
+    password?: string;
+}
+
+export interface DailyReportResponse {
+    e: number;
+    m: string;
+    d: {};
+}
+
+export interface DailyReportForm {
+    ismoved: string;
+    jhfjrq: string;
+    jhfjjtgj: string;
+    jhfjhbcc: string;
+    szgj: string;
+    szcs: string;
+    zgfxdq: string;
+    mjry: string;
+    csmjry: string;
+    ymjzxgqk: string;
+    xwxgymjzqk: string;
+    tw: string;
+    sfcxtz: string;
+    sfjcbh: string;
+    sfcxzysx: string;
+    qksm: string;
+    sfyyjc: string;
+    jcjgqr: string;
+    remark: string;
+    address: string;
+    geo_api_info: string;
+    area: string;
+    province: string;
+    city: string;
+    sfzx: string;
+    sfjcwhry: string;
+    sfjchbry: string;
+    sfcyglq: string;
+    gllx: string;
+    glksrq: string;
+    jcbhlx: string;
+    jcbhrq: string;
+    bztcyy: string;
+    sftjhb: string;
+    sftjwh: string;
+    sfsfbh: string;
+    xjzd: string;
+    jcwhryfs: string;
+    jchbryfs: string;
+    szsqsfybl: string;
+    sfygtjzzfj: number;
+    gtjzzfjsj: string;
+    sfjzxgym: string;
+    sfjzdezxgym: string;
+    jcjg: string;
+    created_uid: number;
+    date: string;
+    uid: string;
+    created: number;
+    id: number;
+}
